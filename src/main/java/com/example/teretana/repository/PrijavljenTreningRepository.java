91c9f991a6fca7a7714154f805acddb9b5bb270e
vujke123 <49960245+vujke123@users.noreply.github.com>
2021-05-29 11:18:27 +0200

    f1: registracija

--- src/main/java/com/example/teretana/repository/PrijavljenTreningRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.teretana.repository;
-
-import com.example.teretana.model.Prijavljen_Trening;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrijavljenTreningRepository extends JpaRepository<Prijavljen_Trening, Long> {
-    
-}
